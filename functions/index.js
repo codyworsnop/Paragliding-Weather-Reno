f4b444fb7b04fd9526bd5a1f4626984e7d6592ce
Cody Worsnop <cworsnop@figure.com>
2021-09-05 16:09:29 -0700

    card content POC

--- functions/index.js
+++ functions/index.js
@@ -4,49 +4,34 @@ const cors = require('cors')({ origin: true });
 const cheerio = require('cheerio');
 const getUrls = require('get-urls');
 const fetch = require('node-fetch');
+const puppeteer = require('puppeteer')
 
-const scrapeMetatags = async (text) => {
+const collect = async (url) => {
+    const browser = await puppeteer.launch( { headless: true });
+    const page = await browser.newPage();
+    
+    await page.goto(url);
+    await page.screenshot({path: '1.png'})
+    await page.waitForSelector('pre')
 
-    const urls = Array.from( getUrls(text) );
+    // Execute code in the DOM
+    const data = await page.evaluate(() => {
 
-    const requests = urls.map(async url => {
-
-        const res = await fetch(url);
-
-        const html = await res.text();
-        const $ = cheerio.load(html);
-        
-        const getMetatag = (name) =>  
-            $(`meta[name=${name}]`).attr('content') ||  
-            $(`meta[name="og:${name}"]`).attr('content') ||  
-            $(`meta[name="twitter:${name}"]`).attr('content');
-
-        return { 
-            url,
-        }
+        const content = document.querySelector('pre').innerHTML
+        return content
     });
-
-
-    return Promise.all(requests);
+  
+    await browser.close();
+    return data;
 }
 
 exports.scraper = functions.https.onRequest( async (request, response) => {
     cors(request, response, async () => {
 
-        console.log(request.body.text)
-      //  const body = JSON.parse(request.body);
-        const data = await scrapeMetatags(request.body.text);
+        console.log(request.body.url)
+        const data = await collect(request.body.url);
 
         response.send(data)
 
     });
 });
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
